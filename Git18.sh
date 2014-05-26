0fecae6fa66d631c9e4ce332c6634c44d093a0a8
Kerry Hatcher <kwhatcher@gmail.com>
2014-05-26 10:26:37 -0400

    Added Git18.sh

---
+++ Git18.sh
@@ -0,0 +1,9 @@
+wget http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/epel-release-6-5.noarch.rpm
+
+yum install epel-release-6-5.noarch.rpm
+
+wget http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/ius-release-1.0-11.ius.el6.noarch.rpm
+
+yum install ius-release-1.0-11.ius.el6.noarch.rpm
+
+yum install git18
